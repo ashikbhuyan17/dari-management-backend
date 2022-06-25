c63ec2c98c8d1449033aaa2ddd363fbd4699144e
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2022-06-25 22:15:52 +0600

    heroku

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 
 const dbConnect = async () => {
-  console.log("DATABASE_NAME", process.env.DATABASE_NAME);
-  mongoose.connect(`mongodb+srv://ashik17:${process.env.PASSWORD}@cluster0.df83a.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    // userCreateIndex: true,
+  // console.log("DATABASE_NAME", process.env.DATABASE_NAME);
+  // mongoose.connect(`mongodb+srv://ashik17:${process.env.PASSWORD}@cluster0.df83a.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   // userCreateIndex: true,
 
-  });
-  const db = mongoose.connection
-  // console.log(db);
-  db.on('error', (err) => {
-    console.log(err);
-    console.log('this is error');
-  })
-  db.once('open', () => {
-    console.log("database connection done");
-  })
+  // });
+  // const db = mongoose.connection
+  // // console.log(db);
+  // db.on('error', (err) => {
+  //   console.log(err);
+  //   console.log('this is error');
+  // })
+  // db.once('open', () => {
+  //   console.log("database connection done");
+  // })
+
+  mongoose
+    .connect(`mongodb+srv://ashik17:${process.env.PASSWORD}@cluster0.df83a.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+    .then(() => console.log('Database Connected'))
+    .catch(err => console.log(err));
 
 };
 
